5e64dd24451fd8922e9008b27f1c91ff988534fe
Ostap Hutsal <97118582+Ostap-04@users.noreply.github.com>
2026-01-05 23:53:54 +0200

    Start project: - connect to db - add simple entity - add Members controller

---
+++ DatingApp.API/Controllers/MembersController.cs
@@ -0,0 +1,37 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class MembersController : Controller
+{
+    private readonly AppDbContext _context;
+    
+    public MembersController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    {
+        var members = await _context.Users.ToListAsync();
+        return members;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetMember(string id)
+    {
+        var member = await _context.Users.FindAsync(id);
+        if (member == null)
+        {
+            return NotFound();
+        }
+        
+        return member;
+    }   
+}
---
+++ DatingApp.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DatingApp.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+    {
+    }
+    
+    public DbSet<AppUser> Users { get; set; }
+}
